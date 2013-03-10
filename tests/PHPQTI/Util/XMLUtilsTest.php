553908853eb7fe3b84edb999eea5b9fc90410f5a
Michael Aherne <micaherne@blueyonder.co.uk>
2013-03-10 22:09:44 +0000

    Removed test which relied on spec XML

--- tests/PHPQTI/Util/XMLUtilsTest.php
+++ tests/PHPQTI/Util/XMLUtilsTest.php
@@ -11,12 +11,6 @@ class XMLUtilsTest extends PHPUnit_Framework_TestCase {
     
     function testUnmarshall() {
         $dom = new \DOMDocument();
-        $dom->load('qtiv2p1/examples/items/choice.xml');
-        $xmlutils = new XMLUtils();
-        $assessmentItem = $xmlutils->unmarshall($dom);
-        $this->assertInstanceOf('PHPQTI\Model\Gen\AssessmentItem', $assessmentItem);
-        $children = $assessmentItem->getChildren('\PHPQTI\Model\ItemBody');
-        $this->assertCount(1, $children);
         
         // Test actual object
         $this->assertTrue($dom->loadXML('<b xmlns="http://www.imsglobal.org/xsd/imsqti_v2p1" id="test" class="nim" label="bim">Hello there!</b>'));
