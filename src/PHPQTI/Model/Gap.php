01fa9e9603e55235f81538101866445a659920ed
Michael Aherne <micaherne@blueyonder.co.uk>
2013-03-11 00:25:20 +0000

    Added existing gapMatchInteraction implementation

--- src/PHPQTI/Model/Gap.php
+++ src/PHPQTI/Model/Gap.php
@@ -8,5 +8,38 @@ class Gap extends \PHPQTI\Model\Gen\Gap implements AssociableChoice {
 
     protected $_elementName = 'gap';
 
+    public function __invoke($controller) {
+        $gapMatchInteraction = $this->findAncestorWithInterface('PHPQTI\Model\Base\Interaction');
+        $identifier = $this->identifier;
+        if (isset($this->required) && $this->required == 'true') {
+            $required = ' required = "required" ';
+        } else {
+            $required = '';
+        }
+        $result = "<span class=\"qti_gap\"><select name=\"{$gapMatchInteraction->responseIdentifier}[{$identifier}]\" $required>";
+        $result .= "<option></option>";
+        foreach($gapMatchInteraction->gapChoice as $choice) {
+            $variable = $controller->response[$gapMatchInteraction->responseIdentifier];
+            $directedPairString = $choice->identifier . ' ' . $identifier;
     
+            // Select correct options if we already have a value (i.e. after end attempt)
+            if (!empty($variable->value)) {
+                if ($variable->cardinality == 'single') {
+                    $selected = ($variable->value ==  $directedPairString ? ' selected="selected"' : '');
+                } else if ($variable->cardinality == 'multiple') {
+                    $selected = (in_array($directedPairString, $variable->value) ? ' selected="selected"' : '');
+                }
+            } else {
+                $selected = '';
+            }
+    
+            $result .= "<option value=\"{$choice->identifier}\" $selected>";
+            foreach($choice->_children as $child) {
+                $result .= $child->__invoke($controller);
+            }
+            $result .= "</option>";
+        }
+        $result .= '</select></span>';
+        return $result;
+    }
 }
--- src/PHPQTI/Model/GapMatchInteraction.php
+++ src/PHPQTI/Model/GapMatchInteraction.php
@@ -12,5 +12,44 @@ class GapMatchInteraction extends \PHPQTI\Model\Gen\GapMatchInteraction
 
     protected $_elementName = 'gapMatchInteraction';
 
+    public function __invoke($controller) {
+        $variableName = $this->responseIdentifier;
+        $result = "<div id=\"gapMatchInteraction_{$variableName}\" class=\"qti_blockInteraction\">";
     
+        // Find variable
+        if (!isset($controller->response[$variableName])) {
+            throw new Exception("Declaration for $variableName not found");
+        }
+    
+        $responseVariable = $controller->response[$variableName];
+    
+        $this->gapChoice = array();
+        // TODO: Implement gapImg
+        $this->fixed = array();
+        $this->displayNodes = array(); // Nodes which will be processed for display as normal
+        // Process child nodes
+        foreach($this->_children as $child) {
+            if ($child instanceof Prompt) {
+                $this->prompt = $child;
+            } else if ($child instanceof GapText) {
+                $this->gapChoice[] = $child;
+                if(isset($child->fixed) && $child->fixed === 'true') {
+                    $this->fixed[] = count($this->gapChoice) - 1;
+                }
+            } else {
+                $this->displayNodes[] = $child;
+            }
+        }
+    
+        if (!is_null($this->prompt)) {
+            $result .= $this->prompt->__invoke($controller);
+        }
+    
+        foreach($this->displayNodes as $node) {
+            $result .= $node->__invoke($controller);
+        }
+    
+        $result .= "</div>";
+        return $result;
+    }
 }
