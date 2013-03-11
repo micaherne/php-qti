bb3e75c99833685d4b3adf6e30c8e9af91ceab0d
Michael Aherne <micaherne@blueyonder.co.uk>
2013-03-11 00:42:27 +0000

    Added old inlineChoiceInteraction implementation

--- src/PHPQTI/Model/InlineChoice.php
+++ src/PHPQTI/Model/InlineChoice.php
@@ -8,5 +8,21 @@ class InlineChoice extends \PHPQTI\Model\Gen\InlineChoice implements Choice {
 
     protected $_elementName = 'inlineChoice';
 
+    public function __invoke($controller) {
+        $identifier = $this->identifier;
     
+        // See if this response was selected already
+    
+        if ($controller->response[$this->name]->value == $identifier) {
+            $selected = ' selected="selected" ';
+        } else {
+            $selected = '';
+        }
+        $result = '<option value="' . $identifier . "\" $selected>";
+        foreach($this->_children as $child) { // should be only one
+            $result .= $child->__invoke($controller);
+        }
+        $result .= '</option>';
+        return $result;
+    }
 }
--- src/PHPQTI/Model/InlineChoiceInteraction.php
+++ src/PHPQTI/Model/InlineChoiceInteraction.php
@@ -1,16 +1,58 @@
 <?php
- 
+
 namespace PHPQTI\Model;
- 
+
 use PHPQTI\Model\Base\Interaction;
 use PHPQTI\Model\Base\Inline;
 use PHPQTI\Model\Base\Flow;
 use PHPQTI\Model\Base\InlineInteraction;
+use PHPQTI\Util\ChoiceIterator;
 
 class InlineChoiceInteraction extends \PHPQTI\Model\Gen\InlineChoiceInteraction
-    implements InlineInteraction, Flow, Inline, Interaction {
+implements InlineInteraction, Flow, Inline, Interaction {
 
     protected $_elementName = 'inlineChoiceInteraction';
 
-    
+    public function __invoke($controller) {
+        $variableName = $this->responseIdentifier;
+        if (isset($this->required) && $this->required == 'true') {
+            $required = ' required = "required" ';
+        } else {
+            $required = '';
+        }
+        $result = "<select name=\"{$variableName}\" id=\"inlineChoiceInteraction_{$variableName}\" class=\"qti_inlineChoiceInteraction\" $required>";
+        // Empty choice
+        $result .= "<option></option>";
+
+        // Find variable
+        if (!isset($controller->response[$variableName])) {
+            throw new Exception("Declaration for $variableName not found");
+        }
+
+        $responseVariable = $controller->response[$variableName];
+
+        $this->inlineChoice = array();
+        $this->fixed = array();
+        // Process child nodes
+        foreach($this->_children as $child) {
+            if ($child instanceof InlineChoice) {
+                $this->inlineChoice[] = $child;
+                $child->name = $variableName;
+                if(isset($child->fixed) && $child->fixed === 'true') {
+                    $this->fixed[] = count($this->inlineChoice) - 1;
+                }
+            } else {
+                throw new Exception("Unknown child element in inlineChoice");
+            }
+        }
+
+        $shuffle = $this->shuffle === 'true';
+        $choiceIterator = new ChoiceIterator($this->inlineChoice, $shuffle);
+        foreach($choiceIterator as $choice) {
+            $result .= $choice->__invoke($controller);
+        }
+
+        $result .= "</select>";
+        return $result;
+    }
 }
