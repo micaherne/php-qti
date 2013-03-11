5ef942225610c93da97e38672b73341ac2437b95
Michael Aherne <michael.aherne@strath.ac.uk>
2013-03-11 17:22:40 +0000

    Added existing uploadInteraction implementation

--- src/PHPQTI/Model/UploadInteraction.php
+++ src/PHPQTI/Model/UploadInteraction.php
@@ -12,5 +12,16 @@ class UploadInteraction extends \PHPQTI\Model\Gen\UploadInteraction
 
     protected $_elementName = 'uploadInteraction';
 
-    
+    public function __invoke($controller) {
+    	$variableName = $this->responseIdentifier;
+    	$result = "<div id=\"uploadInteraction_{$variableName}\" method=\"post\">";
+    	$result .= "<input type=\"file\" name=\"{$variableName}\" >";
+    	foreach($this->_children as $child) {
+    		if ($child instanceof Prompt) {
+    			$result .= "<div class=\"qti_prompt\">" . $child->__invoke($controller) . "</div>";
+    		}
+    	}
+    	$result .= "</div>";
+    	return $result;
+    }
 }
