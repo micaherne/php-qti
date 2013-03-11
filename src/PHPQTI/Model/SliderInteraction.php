5974b709efacd85b7ada0fb60fe494a9bfb06f4d
Michael Aherne <micaherne@blueyonder.co.uk>
2013-03-11 00:49:07 +0000

    Added sliderInteraction implementation

--- src/PHPQTI/Model/SliderInteraction.php
+++ src/PHPQTI/Model/SliderInteraction.php
@@ -7,10 +7,31 @@ use PHPQTI\Model\Base\Flow;
 use PHPQTI\Model\Base\Block;
 use PHPQTI\Model\Base\BlockInteraction;
 
+// TODO: Show min and max labels at either end. Support stepLabel and reverse
 class SliderInteraction extends \PHPQTI\Model\Gen\SliderInteraction 
     implements BlockInteraction, Block, Flow, Interaction {
 
     protected $_elementName = 'sliderInteraction';
 
+    public function __invoke($controller) {
+        $variableName = $this->responseIdentifier;
+        $value = $controller->response[$variableName]->getValue();
+    
+        $result = '';
+    
+        foreach($this->_children as $child) {
+            if ($child instanceof Prompt) {
+                $result .= $child->__invoke($controller);
+            }
+        }
+    
+        $result .= "<div class=\"qti_sliderInteraction\" ";
+        $result .= implode(' ', $this->_getDataAttributes());
+        $result .= "> <div class=\"value\"></div>";
+        $result .= "<div class=\"slider\" /></div> ";
+        $result .= "<input type=\"hidden\" name=\"{$variableName}\" value=\"{$value}\" />";
+        $result .= "</div>";
+        return $result;
+    }
     
 }
