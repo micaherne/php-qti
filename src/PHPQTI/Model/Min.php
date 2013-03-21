a570ff9f202cc72109180775fbee113874177596
Michael Aherne <michael.aherne@strath.ac.uk>
2013-03-21 13:49:13 +0000

    Added QTIVariable declaration to some more class files

--- src/PHPQTI/Model/Min.php
+++ src/PHPQTI/Model/Min.php
@@ -3,6 +3,7 @@
 namespace PHPQTI\Model;
  
 use PHPQTI\Model\Base\Expression;
+use PHPQTI\Runtime\QTIVariable;
 
 class Min extends \PHPQTI\Model\Gen\Min implements Expression {
 
--- src/PHPQTI/Model/RandomFloat.php
+++ src/PHPQTI/Model/RandomFloat.php
@@ -3,6 +3,7 @@
 namespace PHPQTI\Model;
  
 use PHPQTI\Model\Base\Expression;
+use PHPQTI\Runtime\QTIVariable;
 
 class RandomFloat extends \PHPQTI\Model\Gen\RandomFloat implements Expression {
 
