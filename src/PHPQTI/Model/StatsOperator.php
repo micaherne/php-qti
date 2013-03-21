91c5ecddaf324e5c5aecc0402a9f7810f15020c0
Michael Aherne <michael.aherne@strath.ac.uk>
2013-03-21 14:47:25 +0000

    Implemented mean in StatsOperator

--- src/PHPQTI/Model/StatsOperator.php
+++ src/PHPQTI/Model/StatsOperator.php
@@ -3,10 +3,13 @@
 namespace PHPQTI\Model;
  
 use PHPQTI\Model\Base\Expression;
+use PHPQTI\Runtime\QTIVariable;
 
 class StatsOperator extends \PHPQTI\Model\Gen\StatsOperator implements Expression {
 
     protected $_elementName = 'statsOperator';
 
-    
+    public function __invoke($controller) {
+        return QTIVariable::statsOperator($this->name, $this->_children[0]($controller));
+    }
 }
